b0139cb17ddfada398cc8c8be82bd89443667ee6
Brigid Michelle Syondie <brigidmichelle02@gmail.com>
2025-07-21 10:36:42 +0300

    Updated structure

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  root: 'frontend',          
   plugins: [react()],
-})
+  build: {
+    outDir: '../dist',       
+    emptyOutDir: true        
+  },
+  server: {
+    port: 5173,
+    open: true
+  }
+});
