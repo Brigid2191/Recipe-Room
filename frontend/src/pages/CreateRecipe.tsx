a09a189ee8472483467d72eca7bad2ade569ac4b
marylyne <otienomarylyne76@gmail.com>
2025-07-28 15:23:49 +0300

    updates the code

---
+++ frontend/src/pages/CreateRecipe.tsx
@@ -0,0 +1,9 @@
+const CreateRecipe = () => {
+    return (
+        <div className="container mt-5">
+            <h1 className="text-danger">Create Recipe Page</h1>
+        </div>
+    );
+};
+
+export default CreateRecipe;
---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,10 @@
+
+const Login = () => {
+  return (
+    <div className="container mt-5">
+      <h1 className="text-primary">Login Page</h1>
+    </div>
+  );
+};
+
+export default Login;
