c7ba524281b0a0e5bf0143273a97bd1447bd15c2
Emmanuel Ngetich <engetich333@gmail.com>
2025-07-30 03:59:23 -0700

    Merge branch 'main' of github.com:EmmanuelNgetich637/Recipe-Room

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,11 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-<<<<<<< HEAD:frontend/src/api/axios.tsx
+
   baseURL: import.meta.env.VITE_API_URL || "http://127.0.0.1:5000",
-=======
-  baseURL: import.meta.env.VITE_API_URL || "http://127.0.0.1:5000/api",
->>>>>>> 347ee8f653c4e5415a11deb7caa459dedb9593c0:frontend/src/api/axios.ts
+
   headers: {
     "Content-Type": "application/json",
   },
